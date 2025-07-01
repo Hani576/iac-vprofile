5d82b51c3a14ada9cbf493d98b576f67fe8909f9
Hani576 <karim.azizi898@gmail.com>
2025-07-01 16:19:32 +0200

    n. verginia

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,15 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "vprofile-eks"
+  default     = "my-eks"
 }
 
 
 ####
-##ääää
+##hani
