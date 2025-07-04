6def09c37e35ef4de3fa76ea0d507899bf661e81
Hani576 <karim.azizi898@gmail.com>
2025-07-04 16:48:37 +0200

    m-eks-2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "clusterName" {
@@ -11,5 +11,5 @@ variable "clusterName" {
 }
 
 
-####klllll
+####kll
 #
